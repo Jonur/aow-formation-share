3514fad604dad9fa6ade51c6c1b4ec4739e94279
Dimitris Damilos <d.k.damilos@gmail.com>
2020-10-17 19:43:06 +0100

    Add credit links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,27 @@ const Footer = () => (
       href="https://github.com/Jonur"
       alt="Jonur on Github"
     >
-      Jonur
+      Created by Jonur
+    </a>
+    {` - `}
+    <a
+      className={s.link}
+      rel="noopener noreferrer"
+      target="_blank"
+      href="https://github.com/Jonur/aow-formation-share/blob/main/PrivacyPolicy.md"
+      alt="Application Privacy Policy"
+    >
+      Privacy Policy
+    </a>
+    {` - `}
+    <a
+      className={s.link}
+      rel="noopener noreferrer"
+      target="_blank"
+      href="https://github.com/Jonur/aow-formation-share#credits"
+      alt="Credits"
+    >
+      Credit Info
     </a>
   </footer>
 );
