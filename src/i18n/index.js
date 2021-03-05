1486bedc0df363492bad6fddf31be45ac9e39d17
Dimitris Damilos <d.k.damilos@gmail.com>
2021-03-05 18:10:15 +0000

    Add German language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,9 +1,10 @@
 import bg from "./bg.json";
 import cs from "./cs.json";
+import de from "./de.json";
 import en from "./en.json";
 import el from "./el.json";
 import fr from "./fr.json";
 import ko from "./ko.json";
 import pt from "./pt.json";
 
-export default { bg, cs, en, el, fr, ko, pt };
+export default { bg, cs, de, en, el, fr, ko, pt };
