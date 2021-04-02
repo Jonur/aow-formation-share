a2fc08b4fd8888b7919f4bfcfac242654cfe62b7
Dimitris Damilos <d.k.damilos@gmail.com>
2021-04-02 21:35:14 +0100

    Nullify example

--- scripts/addNewTranslations.js
+++ scripts/addNewTranslations.js
@@ -4,16 +4,7 @@ const path = require("path");
 const dirPath = path.resolve(__dirname, "../src/i18n");
 
 const newTranslations = {
-  "app.tabs.statistics": "Formation statistics",
-  "statistics.header.totalHP": "Total formation HP",
-  "statistics.details.totalHP": `The number above includes the total HP of the hero and troops in the
-  formation, as well as fixed-number summons, like the Pirates of a Pirate
-  Ship and the Beast Master's Best Partner. But it does not include the HP
-  of Necromancer's Skeletons, as their number changes during battle.`,
-  "statistics.header.formation.count": "Troop and Hero count",
-  "statistics.header.formation.analysis": "Troop formation analysis",
-  "statistics.header.formation.races": "Races in the formation",
-  "statistics.header.formation.grades": "Grades in the formation",
+  key: "Value",
 };
 
 const languages = ["ar", "bg", "cs", "de", "en", "el", "fr", "ko", "pt"];
