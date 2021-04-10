95491fd1b7240eb67e6679eba262f46d2ffd0712
Dimitris Damilos <d.k.damilos@gmail.com>
2021-04-10 15:18:43 +0100

    Test CORS 2

--- src/utils/getTinyURL.js
+++ src/utils/getTinyURL.js
@@ -1,6 +1,18 @@
 const getTinyURL = async (longURL) => {
+  let headers = new Headers();
+
+  headers.append(
+    "Access-Control-Allow-Origin",
+    "https://aowformationshare.jonur.io"
+  );
+  headers.append("Access-Control-Allow-Methods", "GET");
+  headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   const tinyURL = await fetch(
-    `https://jonurmail.000webhostapp.com/tiny.php?longUrl=${longURL}`
+    `https://jonurmail.000webhostapp.com/tiny.php?longUrl=${longURL}`,
+    {
+      headers,
+    }
   );
   console.log({ tinyURL });
 };
